cb55d9833e0bb53a2fb6b2b3f3cad8036690b41f
Ashwin S Shenoy <aswinshenoy65@gmail.com>
2019-12-03 13:25:25 +0530

    Add static Path Declaration

--- next.config.js
+++ next.config.js
@@ -3,8 +3,29 @@ const sass = require('@zeit/next-sass');
 const css = require('@zeit/next-css');
 const OptimizedImages = require('next-optimized-images');
 const videos = require('next-videos');
+const fetch = require('isomorphic-unfetch');
 
 const withSourceMaps = require( '@zeit/next-source-maps' );
+
+
+const nextConfig = {
+    exportTrailingSlash: true,
+    exportPathMap: async function() {
+        const paths = {
+            '/': {page: '/'},
+            '/admin': {page: '/admin'},
+            '/competitions': {page: '/competitions'},
+            '/dashboard': {page: '/dashboard'},
+            '/login': {page: '/login'},
+            '/logout': {page: '/logout'},
+            '/shows': {page: '/shows'},
+            '/purchase': {page: '/purchase'},
+            '/workshops': {page: '/workshops'},
+        };
+
+        return paths;
+    }
+};
 
 module.exports = withPlugins([
     [withSourceMaps],
@@ -14,4 +35,4 @@ module.exports = withPlugins([
     [sass, {
         test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2|mp4)$/,
     }],
-]);
+], nextConfig);
