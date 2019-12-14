34247fc474c000e65dc82b13a28698493cc62085
Ashwin S Shenoy <aswinshenoy65@gmail.com>
2019-12-14 14:57:48 +0530

    Design Improvements in Edit Profile Page

--- modules/profile/collegeIDCard.js
+++ modules/profile/collegeIDCard.js
@@ -1,11 +1,16 @@
 import React from "react";
+import Link from "next/link";
 
 const CollegeIDPhoto = ({ collegeIDPhoto }) => {
 
     const renderNoProfilePhotoFound = (
-        <div className="bg-white p-4 h-100">
-            <h5>No College ID Found</h5>
-            <p>A photo of your face is required to enter Vidyut 2020, for security reasons</p>
+        <div className="bg-white p-4 h-100 d-flex align-items-center text-center justify-content-center">
+            <div>
+                <img src={require('../../images/icons/selfie.png')} style={{ width: "120px", marginBottom: "1rem" }} />
+                <h5>College ID Card Not Uploaded</h5>
+                <p>Your college ID card is required as a proof for you being a student.</p>
+                <Link href="/profile/upload-collegeID"><button className="btn px-4 py-2 rounded-0 ">Upload College ID</button></Link>
+            </div>
         </div>
     );
 
--- modules/profile/profilePicture.js
+++ modules/profile/profilePicture.js
@@ -1,11 +1,16 @@
 import React from "react";
+import Link from "next/link";
 
 const ProfilePicture = ({ profilePhoto }) => {
 
     const renderNoProfilePhotoFound = (
-        <div className="bg-white p-4 h-100">
-            <h5>No Selfie Found</h5>
-            <p>A photo of your face is required to enter Vidyut 2020, for security reasons</p>
+        <div className="bg-white p-4 h-100 d-flex align-items-center text-center justify-content-center">
+            <div>
+                <img src={require('../../images/icons/selfie.png')} style={{ width: "120px", marginBottom: "1rem" }} />
+                <h5>Selfie Not Uploaded</h5>
+                <p>A photo of your face is required to enter Vidyut 2020, for security reasons</p>
+                <Link href="/profile/upload-selfie"><button className="btn px-4 py-2 rounded-0">Upload Selfie</button></Link>
+            </div>
         </div>
     );
 
